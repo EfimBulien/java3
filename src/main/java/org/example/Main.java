5a7898b81f6a714307e5303da854d09046be73e5
Ефим Бульен <143902970+EfimBulien@users.noreply.github.com>
2024-02-12 16:19:21 +0300

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,25 @@
 package org.example;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        // Создание экземпляра класса
         Scanner sc = new Scanner(System.in);
-
+        // Переменная для выхода из цикла и повторения
         boolean repeat = true;
+        // Цикл с повторением выполенения программы
         while (repeat){
             System.out.println("Введите три целых числа:");
+            // Запись в переменную стороны а
             int a = sc.nextInt();
             sc.nextLine();
+            // Запись в переменную стороны b
             int b = sc.nextInt();
             sc.nextLine();
+            // Запись в переменную стороны c
             int c = sc.nextInt();
             sc.nextLine();
-
+            // Тернарное выражение в println
             System.out.println((a * a + b * b == c * c || b * b + c * c == a * a || c * c + a * a == b * b) ?
                     (a + ", " + b + " и " + c + " являются сторонами прямоугольного треугольника") :
                     (a == b && b == c) ?
@@ -27,9 +31,12 @@ public class Main {
                                             (a + ", " + b + " и " + c + " являются сторонами обычного треугольника"));
 
             System.out.println("Желаете выполнить программу еще раз? (1 - да, 0 - нет)");
+            //Выбор по работе программы
             int choice = sc.nextInt();
+            //Условие выбора (сравнение с единицей)
             repeat = (choice == 1);
         }
+        //Закрытие сканера
         sc.close();
     }
 }
